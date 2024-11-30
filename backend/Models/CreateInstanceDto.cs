766a3089a22448716da1c3f7ac1ff92efb407c55
Everton <evertonvendramine@gmail.com>
2024-11-30 19:59:20 -0300

    método criar instacia  configurado

---
+++ backend/Models/CreateInstanceDto.cs
@@ -0,0 +1,13 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace backend.Models
+{
+    public class CreateInstanceDto
+    {
+        public string instanceName { get; set; }
+        public bool qrCode { get; set; }
+        public string integration { get; set; }
+    }
+    
+
+}
--- backend/Program.cs
+++ backend/Program.cs
@@ -1,5 +1,7 @@
+using backend.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
 
 namespace backend
 {
@@ -9,6 +11,7 @@ namespace backend
         {
             var builder = WebApplication.CreateBuilder(args);
             builder.Services.AddControllers();
+            builder.Services.AddHttpClient();
             builder.Services.AddCors(options =>
             {
                 options.AddPolicy("AllowAll", policy =>
@@ -19,6 +22,39 @@ namespace backend
                 });
             });
             var app = builder.Build();
+            app.UseStaticFiles();
+
+            app.MapPost("api/createinstance", async (CreateInstanceDto dto, IHttpClientFactory httpClientFactory) =>
+            {
+                
+                var client = httpClientFactory.CreateClient();
+
+                
+                client.DefaultRequestHeaders.Add("apikey", "wtwHLYfFxI9n1zDR8zFFqNq8kVaWqdD2oLpcjVmXBu");
+                client.DefaultRequestHeaders.Add("User-Agent", "PixPaymentIntegrationTest");
+
+                
+                var postForm = new FormUrlEncodedContent(new[]
+                {
+        
+                 new KeyValuePair<string, string>("instanceName", dto.instanceName),
+                 new KeyValuePair<string, string>("qrCode", dto.qrCode.ToString()),
+                 new KeyValuePair<string, string>("integration", dto.integration),
+
+    });
+
+                
+                var response = await client.PostAsync("https://evolutionzap.apievolution.shop/instance/create", postForm);
+                if (!response.IsSuccessStatusCode)
+                {
+                    
+                    return Results.BadRequest(response);
+                }
+
+                
+                return Results.Ok(response);
+            });
+            app.UseHttpsRedirection();
             app.UseCors("AllowAll");
             app.MapControllers();
             app.Run();
