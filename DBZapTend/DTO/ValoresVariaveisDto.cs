2c075f6dc42a4bfa348e969278bea2f3c586fb07
Everton <evertonvendramine@gmail.com>
2025-01-15 17:09:04 -0300

    Add Dtos

---
+++ DBZapTend/DTO/ValoresVariaveisDto.cs
@@ -0,0 +1,15 @@
+﻿namespace DBZapTend.DTO
+{
+    public class ValoresVariaveisDto
+    {
+        public int IdValoresVariaveis { get; set; }
+
+        public string UserId { get; set; }
+
+        public int? PromptsIdPrompts { get; set; }
+
+        public string Value { get; set; }
+
+        public int? VariaveisIdVariaveis { get; set; }
+    }
+}
---
+++ DBZapTend/DTO/VariaveisDto.cs
@@ -0,0 +1,13 @@
+﻿namespace DBZapTend.DTO
+{
+    public class VariaveisDto
+    {
+        public int IdVariaveis { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public int? PromptsIdPrompts { get; set; }
+    }
+}
